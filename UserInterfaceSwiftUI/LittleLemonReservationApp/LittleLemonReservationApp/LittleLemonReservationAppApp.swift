97a14412db82583ee1f8feb14c46ae6005f40e7e
NAbin RAi <nabinr@gmeremit.com>
2023-04-23 16:55:43 +0545

    swift ui and core data3

---
+++ UserInterfaceSwiftUI/LittleLemonReservationApp/LittleLemonReservationApp/LittleLemonReservationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LittleLemonReservationAppApp.swift
+//  LittleLemonReservationApp
+//
+//  Created by NAbin RAi on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct LittleLemonReservationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ UserInterfaceSwiftUI/LittleLemonReservationApp/LittleLemonReservationApp/LitttleLemonLogo.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        Image("little-lemon-logo")
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
+
+
